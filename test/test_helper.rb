60e27a0f5bb8c9193170047379e3761c3a5baaf0
duckduckgoose01 <davidshapiro@brandeis.edu>
2022-10-06 13:13:02 -0400

    loading starting material

---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  include ApplicationHelper
+
+  # Add more helper methods to be used by all tests here...
+end
