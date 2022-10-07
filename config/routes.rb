7c5a4cf616716f4553f09b418401fa67f8bbfc6f
duckduckgoose01 <davidshapiro@brandeis.edu>
2022-10-06 21:30:02 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
   get '/signup', to: "users#new"
+  resources :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#home"
